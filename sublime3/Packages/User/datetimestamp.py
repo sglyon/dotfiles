26eb60e0d6a6868b899915cde45066f5a39d972e
Spencer Lyon <spencerlyon2@gmail.com>
2013-08-23 10:57:53 -0700

    PY: Added python rc files (ipython and matplotlib)

---
+++ sublime3/Packages/User/datetimestamp.py
@@ -0,0 +1,14 @@
+import datetime, getpass
+import sublime, sublime_plugin
+
+class AddDateTimeStampCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("insert_snippet", { "contents": "%s" %  datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") } )
+
+class AddDateStampCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("insert_snippet", { "contents": "%s" %  datetime.datetime.now().strftime("%Y-%m-%d") } )
+
+class AddTimeStampCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("insert_snippet", { "contents": "%s" %  datetime.datetime.now().strftime("%H:%M:%S") } )
