ede00603064829975c1f4452f920a1730da2874b
Spencer Lyon <spencerlyon2@gmail.com>
2013-12-21 14:00:56 -0700

    SUBL3: just another update

--- Julia/juliarc.jl
+++ Julia/juliarc.jl
@@ -1,12 +1,35 @@
 function addprocs_odyny(n::Int)
+    """
+    Add processes on the desktop in my parents nyc apartment
+
+    """
     addprocs(repmat(["sglyon@odyny.homeip.net:22"], n),
              tunnel=true,
-             dir="/Users/sglyon/.src/julia/usr/bin")
+             dir='/usr/local/julia/usr/bin')
 end
 
 
 function addprocs_home(n::Int)
+    """
+    Add processes on my home desktop
+
+    """
     addprocs(repmat(["sglyon@sglyon.selfip.org:22"], n),
              tunnel=true,
-             dir="/Users/spencerlyon2/.src/julia/usr/bin")
+             dir='/usr/local/julia/usr/bin')
+end
+
+
+macro run(x::Symbol)
+    """
+    Reloads the script ``x.jl``. The file name should be given without
+    the ``.jl`` extension. So, for example, if I wanted to run
+    ``foo.jl`` I would call ``@run foo``
+
+    Notes
+    -----
+    Should feel like the IPython %run magic
+
+    """
+    reload(string(x) * ".jl")
 end
